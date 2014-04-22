207f3a0f17b05ccae46e3f157ff7b4c91b441954
zackattacknyu <zackattacknyu@gmail.com>
2014-04-21 17:20:57 -0700

    progress on prob 6

--- Homework2/prob6scriptPics.m
+++ Homework2/prob6scriptPics.m
@@ -1,4 +1,4 @@
-picname = 'zebra1.jpg';
+picname = 'zebraMod.jpg';
 imageData = rgb2gray(imread(picname));
 bwPicName = strcat('bw_',picname);
 imwrite(imageData,bwPicName,'JPEG');
@@ -14,4 +14,22 @@ phaseSpectrumImage = angle(imageFFT);
 figure
 imagesc(phaseSpectrumImage);
 title(strcat('Phase Spectrum Image: ',picname));
+colorbar;
+
+pic2name = 'simpsons.jpg';
+image2Data = rgb2gray(imread(pic2name));
+bwPic2Name = strcat('bw_',pic2name);
+imwrite(image2Data,bwPic2Name,'JPEG');
+image2FFT = fft2(image2Data);
+
+magSpectrumImage2 = abs(image2FFT);
+figure
+imagesc(magSpectrumImage2);
+title(strcat('Magnitude Spectrum Image: ',pic2name));
+colorbar;
+
+phaseSpectrumImage2 = angle(image2FFT);
+figure
+imagesc(phaseSpectrumImage2);
+title(strcat('Phase Spectrum Image: ',pic2name));
 colorbar;
