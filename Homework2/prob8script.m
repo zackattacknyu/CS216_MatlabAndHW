d75965a88f276dc07d3c50a9f2fa381e51d1bb81
zackattacknyu <zackattacknyu@gmail.com>
2014-04-24 13:42:35 -0700

    progress on problem 8

--- Homework2/prob8script.m
+++ Homework2/prob8script.m
@@ -7,25 +7,27 @@ imageData = im2double(imread(imageName));
 %imshow(imageData)
 
 %used to get the corner values of the square we care about 
-%[xVals,yVals] = ginput(4)
+%[xVals,yVals] = ginput(2)
 
-%values for the "N" letter
-rightX = 334;
-leftX = 323;
-bottomY = 69;
-topY = 82;
+%values for the word "Nine"
+rightX = 368;
+leftX = 322;
+bottomY = 32;
+topY = 46;
 
 nLetterImageData = imageData(bottomY:topY,leftX:rightX);
-imwrite(nLetterImageData,'nLetter1_dilbert1.jpg','JPEG');
+imwrite(nLetterImageData,'nine_word_dilbert1.jpg','JPEG');
 
-correlation = xcorr2(imageData,nLetterImageData);
+%correlation = xcorr2(imageData,nLetterImageData);
 %correlation = xcorr2(nLetterImageData,imageData);
+correlation = conv2(imageData,flipud(fliplr(nLetterImageData)),'same');
 imagesc(correlation);
 colorbar;
 
 %does the thresholding
 %this is the 1-D example. It needs to be extended to 2D
-threshold = mean(correlation(:));
+%threshold = mean(correlation(:)); %this is currently 387
+threshold = 450;
 L = (correlation(2:end-1,2:end-1) > correlation(1:end-2,2:end-1));
 R = (correlation(2:end-1,2:end-1) > correlation(3:end,2:end-1));
 
@@ -42,16 +44,22 @@ LR = (correlation(2:end-1,2:end-1) > correlation(3:end,3:end));
 T = (correlation(2:end-1,2:end-1) > threshold);
 maxima = R & L & T & UL & UM & UR & LL & LM & LR;
 
+%preps data for the overlay
+%  for the grayscale to rgb, all the color channels are the same
+imageDataSize = size(imageData);
+rgbImageData = zeros(imageDataSize(1),imageDataSize(2),3);
+rgbImageData(:,:,1) = imageData;
+rgbImageData(:,:,2) = imageData;
+rgbImageData(:,:,3) = imageData;
+maximaInverted = 1-maxima;
+
+%puts red dots at the maxima points
+%  this is done by zeroing each channel at a maxima point
+%  For the red channel, zeros are put at the maxima points 
+%       and then a 1 value is put there
+rgbImageData(2:end-1,2:end-1,1) = imageData(2:end-1,2:end-1).*maximaInverted + maxima;
+rgbImageData(2:end-1,2:end-1,2) = imageData(2:end-1,2:end-1).*maximaInverted;
+rgbImageData(2:end-1,2:end-1,3) = imageData(2:end-1,2:end-1).*maximaInverted;
+
 figure
-imagesc(maxima);
-colorbar;
-
-
-%values for the other "N" letter
-rightX = 334;
-leftX = 323;
-bottomY = 32;
-topY = 45;
-
-nLetterImageData2 = imageData(bottomY:topY,leftX:rightX);
-imwrite(nLetterImageData,'nLetter2_dilbert1.jpg','JPEG');
+imshow(rgbImageData);
