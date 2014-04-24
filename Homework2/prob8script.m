d25294b041ae5554fabd97e345dcf07b07248856
zackattacknyu <zackattacknyu@gmail.com>
2014-04-23 22:58:44 -0700

    progress on prob8

--- Homework2/prob8script.m
+++ Homework2/prob8script.m
@@ -26,11 +26,25 @@ colorbar;
 %does the thresholding
 %this is the 1-D example. It needs to be extended to 2D
 threshold = mean(correlation(:));
-L = (correlation(2:end-1) > correlation(1:end-2));
-R = (correlation(2:end-1) > correlation(3:end));
-T = (correlation(2:end-1) > threshold);
-maxima = R & L & T;
+L = (correlation(2:end-1,2:end-1) > correlation(1:end-2,2:end-1));
+R = (correlation(2:end-1,2:end-1) > correlation(3:end,2:end-1));
 
+%upper left, right, and middle ones
+UL = (correlation(2:end-1,2:end-1) > correlation(1:end-2,1:end-2));
+UM = (correlation(2:end-1,2:end-1) > correlation(2:end-1,1:end-2));
+UR = (correlation(2:end-1,2:end-1) > correlation(3:end,1:end-2));
+
+%lower left, right, and middle ones
+LL = (correlation(2:end-1,2:end-1) > correlation(1:end-2,3:end));
+LM = (correlation(2:end-1,2:end-1) > correlation(2:end-1,3:end));
+LR = (correlation(2:end-1,2:end-1) > correlation(3:end,3:end));
+
+T = (correlation(2:end-1,2:end-1) > threshold);
+maxima = R & L & T & UL & UM & UR & LL & LM & LR;
+
+figure
+imagesc(maxima);
+colorbar;
 
 
 %values for the other "N" letter
