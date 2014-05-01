6cb4f5b8ae7340bdd838f4645c0582df517fb7a9
zackattacknyu <zackattacknyu@gmail.com>
2014-05-01 13:56:10 -0700

    continued prob1 work

--- Homework3/prob1script.m
+++ Homework3/prob1script.m
@@ -1,15 +1,29 @@
-imageData = im2double(imread('zebra_small.jpg'));
+imname = 'zebra_small.jpg';
+imageData = im2double(imread(imname));
 imageDataSize = size(imageData);
 numRow = imageDataSize(1);
 numColumn = imageDataSize(2);
 numPixels = numRow*numColumn;
 imageDataRow = reshape(imageData,[numPixels 3]);
+imageDataRowRedEn = reshape(imageData,[numPixels 3]);
+imageDataRowRedEn(:,1) = imageDataRowRedEn(:,1)*100;
 
 %k-Means cluster
 k=5;
 [Indices,Colors] = kmeans(imageDataRow,k);
+[IndicesRedEn,ColorsRedEn] = kmeans(imageDataRowRedEn,k);
 newImageDataRow = zeros([numPixels 3]);
+newImageDataRowRedEn = zeros([numPixels 3]);
 for row = 1:numPixels
    newImageDataRow(row,:) = Colors(Indices(row),:);
+   newImageDataRowRedEn(row,:) = ColorsRedEn(IndicesRedEn(row),:);
 end
 newImageData = reshape(newImageDataRow,[numRow numColumn 3]);
+newImageDataRowRedEn(1,:) = newImageDataRowRedEn(1,:)./100;
+newImageDataRedEn = reshape(newImageDataRowRedEn,[numRow numColumn 3]);
+
+imageTitle = strcat(num2str(k),'-means_',imname);
+imwrite(newImageData,imageTitle,'JPEG');
+
+imageTitleRedEn = strcat(num2str(k),'-means_redEn_',imname);
+imwrite(newImageDataRedEn,imageTitleRedEn,'JPEG');
