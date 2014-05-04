40b41be6c597bc81157f30886b8a66225bbd9be6
zackattacknyu <zackattacknyu@gmail.com>
2014-05-03 19:34:53 -0700

    progress on problem 3

---
+++ Homework3/prob3function.m
@@ -0,0 +1,22 @@
+function [] = prob3function( imageData, centerX,centerY,radius )
+%PROB3FUNCTION Summary of this function goes here
+%   Detailed explanation goes here
+imagePatch = imageData( (centerY-radius/2):(centerY+radius/2),...
+    (centerX-radius/2):(centerX+radius/2) );
+figure
+imshow(imagePatch);
+
+responses = zeros(radius+1,radius+1,8);
+[responses(:,:,1),responses(:,:,2),responses(:,:,3),...
+    responses(:,:,4),responses(:,:,5),responses(:,:,6),...
+    responses(:,:,7),responses(:,:,8)] = ...
+    get8FilterImages(imagePatch,'zebra_small_patch.jpg');
+
+meanResponse = mean(responses,3);
+figure
+imshow(meanResponse);
+figure
+bar(meanResponse,10);
+
+end
+
---
+++ Homework3/prob3script.m
@@ -0,0 +1,19 @@
+imageData = imread('zebra_small.jpg');
+
+%captures the neck
+centerX = 90;
+centerY = 60;
+radius = 40;
+prob3function(imageData,centerX,centerY,radius);
+
+%the tree leaves above the zebra's back
+centerX = 165;
+centerY = 20;
+radius = 30;
+prob3function(imageData,centerX,centerY,radius);
+
+%grass in front of the zebra
+centerX = 136;
+centerY = 224;
+radius = 20;
+prob3function(imageData,centerX,centerY,radius);
