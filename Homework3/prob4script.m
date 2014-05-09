892620e777ef853e12d29fe8d1a6be68946c92e8
zackattacknyu <zackattacknyu@gmail.com>
2014-05-08 20:54:32 -0700

    updated scripts after prob 2 changes

--- Homework3/prob4script.m
+++ Homework3/prob4script.m
@@ -1,23 +1,3 @@
 imageData = im2double(rgb2gray(imread('zebra_small.jpg')));
 
 prob4function(imageData);
-
-%{
-%captures the neck
-centerX = 90;
-centerY = 60;
-radius = 40;
-prob4function(imageData,centerX,centerY,radius);
-
-%the tree leaves above the zebra's back
-centerX = 165;
-centerY = 20;
-radius = 30;
-prob4function(imageData,centerX,centerY,radius);
-
-%grass in front of the zebra
-centerX = 136;
-centerY = 224;
-radius = 20;
-prob4function(imageData,centerX,centerY,radius);
-%}
