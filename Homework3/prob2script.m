363b9b1a6210f407910fea821b4fc821dce672c9
zackattacknyu <zackattacknyu@gmail.com>
2014-05-08 20:51:51 -0700

    finished problem 2

--- Homework3/prob2script.m
+++ Homework3/prob2script.m
@@ -5,7 +5,7 @@ imageData = im2double(rgb2gray(imread(imname)));
 %h1 means horizontal derivative, sigma 1
 %h2, h4, v1, v2, v4 follow the same thing
 %g42 is gaussian 4-2, g21 is similar
-[h1,v1,h2,v2,h4,v4,g42,g21] = get8FilterImages(imageData,imname);
+[h1,v1,h2,v2,h4,v4,g42,g21] = get8FilterImages(imageData);
 figure
 
 subplot(2,4,1)
