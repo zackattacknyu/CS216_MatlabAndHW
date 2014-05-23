9c0076309ecf695b7b5124f1a904acf5354cfe5c
zackattacknyu <zackattacknyu@gmail.com>
2014-05-22 20:32:10 -0700

    fixed small bug with detect function

--- Homework4/detect.m
+++ Homework4/detect.m
@@ -1,5 +1,5 @@
 
-function [x,y,score] = detection(I,template,ndet)
+function [x,y,score] = detect(I,template,ndet)
 %
 % return top ndet detections found by applying template to the given image.
 %   x,y should contain the coordinates of the detections in the image
