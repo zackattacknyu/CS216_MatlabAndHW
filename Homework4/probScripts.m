283433ac6b380042e942833fcce0ce9648d5cfc1
zackattacknyu <zackattacknyu@gmail.com>
2014-05-23 00:12:12 -0700

    most of the results have been compiled

--- Homework4/probScripts.m
+++ Homework4/probScripts.m
@@ -40,10 +40,10 @@ end
 
 %% Problem 3 Script
 
-Itrain = im2double(rgb2gray(imread('test4train.jpg')));
+Itrain = im2double(rgb2gray(imread('test1train.jpg')));
 
 template = hog(Itrain);
-Itest = im2double(rgb2gray(imread('test4.jpg')));
+Itest = im2double(rgb2gray(imread('test1.jpg')));
 currentTest= Itest;
 resizeFactor = 0.7;
 properTest = size(Itrain)<size(currentTest);
